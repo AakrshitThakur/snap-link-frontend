0d4507a777d8d7443f2daabd7588525e284b7593
AakrshitThakur <thakurraakrshitt@gmail.com>
2025-10-04 21:27:10 +0530

    css for zoom out added

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -22,7 +22,7 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div
       id="app"
-      className="h-screen grid grid-cols-1 grid-rows-[0fr_1fr] md:grid-rows-none md:grid-cols-[16rem_1fr] flex-col relative overflow-y-auto"
+      className="h-screen w-full max-w-7xl grid grid-cols-1 grid-rows-[0fr_1fr] md:grid-rows-none md:grid-cols-[16rem_1fr] flex-col relative overflow-y-auto"
     >
       {!isShareableContents ? (
         <>
